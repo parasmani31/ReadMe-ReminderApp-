48837d2c4803cb14231d190688934bd4cbb4fcc9
parasmani31 <93341621+parasmani31@users.noreply.github.com>
2021-10-29 00:27:09 +0530

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Business_Logic_Layer
+{
+    /// <summary>
+    /// This class contains IO related variables, that you don't want to declare in each class.
+    /// </summary>
+    public class IOVariables
+    {
+        private IOVariables() { }
+
+        /// <summary>
+        /// Contains the path to the SQLite Database of RemindMe
+        /// </summary>
+        public static readonly string databaseFile = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\RemindMe\\RemindMeDatabase.db";
+        
+        
+        /// <summary>
+        /// Contains the path to the root folder of RemindMe
+        /// </summary>
+        public static readonly string rootFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\RemindMe\";              
+
+        /// <summary>
+        /// Contains the path to the error log file of RemindMe. This contains exceptions and when they occured.
+        /// </summary>
+        public static readonly string errorLog = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\RemindMe\ErrorLog.txt";
+
+        /// <summary>
+        /// Contains the path to the batch file for advanced reminders. Batch scripts will be written to this and executed
+        /// </summary>
+        public static readonly string batchFile = System.IO.Path.GetTempPath() + "\\RemindMe\\batchscript.bat";
+
+        /// <summary>
+        /// Contains the path to the batch file for advanced reminders. Batch scripts will be written to this and executed
+        /// </summary>
+        public static readonly string uniqueString = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\RemindMe\\string.txt";
+
+        /// <summary>
+        /// Contains the path to the system log of RemindMe. Contains a log of what happened within RemindMe
+        /// </summary>
+        public static readonly string systemLog = System.IO.Path.GetTempPath() + "\\RemindMe\\SystemLog.txt";
+
+        /// <summary>
+        /// Returns the version of RemindMe. Read from the assembly
+        /// </summary>
+        public static string RemindMeVersion
+        {
+            get
+            {
+                System.Reflection.Assembly assembly = Assembly.GetEntryAssembly();
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+                
+                return fvi.FileVersion;
+            }
+        }
+
+    }
+}
