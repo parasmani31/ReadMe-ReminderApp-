0a1d4e6fc0495d74559452f961cb7fb56c0f56ef
parasmani31 <93341621+parasmani31@users.noreply.github.com>
2021-10-29 00:27:46 +0530

    Create reminderexception.cs

---
+++ reminderexception.cs
@@ -0,0 +1,31 @@
+using Database.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer
+{
+    /// <summary>
+    /// The Exception that is thrown when something is bad with an Reminder.
+    /// </summary>
+    public class ReminderException : Exception
+    {
+        /// <summary>
+        /// An exception that is caused by a faulty reminder
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <param name="rem">The reminder that caused an exception</param>       
+        public ReminderException(string message, Reminder rem) : base(message)
+        {
+            Reminder = rem;                        
+        }
+        /// <summary>
+        /// The Reminder object of this Exception
+        /// </summary>
+        public Reminder Reminder { get; private set; }
+
+        public string StackTrace { get; set; }
+    }
+}
